cb54519cc87158a5f09a5d2ba21fd72f1c844d6d
wjc74751 <85719815+wjc74751@users.noreply.github.com>
2024-02-04 20:12:08 +0000

    Add files via upload

--- Hough_Transform.py
+++ Hough_Transform.py
@@ -10,14 +10,14 @@ line2 = - 2*x + 20
 data = np.column_stack((x, line1))
 data = np.vstack((data, np.column_stack((x, line2))))
 
-hough_space = np.zeros((60, 60))
+hough_space = np.zeros((60,64))
 
 
 for n in range(len(data)):
-    for i in np.arange(0, np.pi, 0.01):
-        for j in range(-30,30):
-            if (abs(data[n,0]*np.sin(i) - data[n,1]*np.cos(i) + j) < 0.1): # quantization error
-                hough_space[int(i*10), j+30] += 1
+    for z in np.arange(-30,30,1):
+        for theta in np.arange(0,np.pi,0.05):
+            if (abs(data[n,0]*np.sin(theta) - data[n,1]*np.cos(theta) + z) < 0.1): # quantization error
+                hough_space[z+30,int(theta*20)] += 1 # streth the theta axis by 20
  
 
 
