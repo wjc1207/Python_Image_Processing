bfea4432127f4498851164ebc1e1688bfc30639c
Junchi Wang <85719815+wjc1207@users.noreply.github.com>
2024-07-21 19:47:57 +0800

    Add files via upload

--- wavelet_transform_example.py
+++ wavelet_transform_example.py
@@ -1,8 +1,8 @@
 # Wavelet transform example
 # sudden edge detection
 # Author: Junchi Wang
-# Date: 2024/3/9
-# Version: v1.0
+# Date: 2024/07/21
+# Version: v1.1
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,31 +11,34 @@ import matplotlib.pyplot as plt
 def wavelet_transform(x, wavelet):
     n = len(x)
     m = len(wavelet)
-    y = np.zeros(n)
+    y = np.zeros(n,dtype=np.complex_)
     for i in range(n):
         for j in range(m):
             if i - j >= 0 and i - j < n:
                 y[i] += wavelet[j] * x[i - j]
     return y
 
-omega = 0.1
+delta = 0.1
 N = 100
-x = np.array(np.sin(np.arange(0, N * omega, omega)))
-omega_values = np.arange(0.2, 1, 0.1)
-for omega in omega_values:
-    x = np.append(x, np.array(np.sin(np.arange(0, N * omega, omega))))
-wavelet_omega = 0.5
-wavelet = np.sin(np.arange(-N * wavelet_omega / 2, N * wavelet_omega / 2, wavelet_omega)) * np.exp(-np.power(np.arange(-N / 2, N / 2)/20,2)/2)
+# generate sine wave with increasing frequency
+x = np.array(np.sin(np.arange(0, N * delta, delta)))
+delta_values = np.arange(0.2, 1, 0.1)
+for delta in delta_values:
+    x = np.append(x, np.array(np.sin(np.arange(0, N * delta, delta))))
+# Morlet wavelet
+wavelet_delta = 0.6
+wavelet = np.exp(1j*np.arange(-N * wavelet_delta / 2, N * wavelet_delta / 2, wavelet_delta)) * np.exp(-np.power(np.arange(-N / 2, N / 2)/30,2)/2)
 y = wavelet_transform(x, wavelet)
 
+# plot
 plt.subplot(3, 1, 1)
 plt.plot(x, label='x')
 plt.legend()
 plt.subplot(3, 1, 2)
-plt.plot(y, label='y')
-plt.legend()  
-plt.subplot(3, 1, 3)
 plt.plot(wavelet, label = "wavelet")
 plt.legend()
+plt.subplot(3, 1, 3)
+plt.plot(abs(y), label='y')
+plt.legend()  
 
 plt.show()
