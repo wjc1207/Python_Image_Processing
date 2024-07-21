8c48480706364996d225bbd1cd8b3f6198846e24
Junchi Wang <85719815+wjc1207@users.noreply.github.com>
2024-07-21 17:17:55 +0800

    Add files via upload

--- wavelet_transform_example.py
+++ wavelet_transform_example.py
@@ -18,20 +18,24 @@ def wavelet_transform(x, wavelet):
                 y[i] += wavelet[j] * x[i - j]
     return y
 
-x = np.zeros(50)
-x = np.append(x, np.ones(100))
-x = np.append(x, np.zeros(100))
-x = np.append(x, np.sin(np.linspace(0, 4 * np.pi, 200)))
-noise = np.random.normal(0, 0.1, len(x))
-x += noise
-wavelet = np.array([1, -1])
+omega = 0.1
+N = 100
+x = np.array(np.sin(np.arange(0, N * omega, omega)))
+omega_values = np.arange(0.2, 1, 0.1)
+for omega in omega_values:
+    x = np.append(x, np.array(np.sin(np.arange(0, N * omega, omega))))
+wavelet_omega = 0.5
+wavelet = np.sin(np.arange(-N * wavelet_omega / 2, N * wavelet_omega / 2, wavelet_omega)) * np.exp(-np.power(np.arange(-N / 2, N / 2)/20,2)/2)
 y = wavelet_transform(x, wavelet)
 
-plt.subplot(2, 1, 1)
+plt.subplot(3, 1, 1)
 plt.plot(x, label='x')
 plt.legend()
-plt.subplot(2, 1, 2)
+plt.subplot(3, 1, 2)
 plt.plot(y, label='y')
 plt.legend()  
+plt.subplot(3, 1, 3)
+plt.plot(wavelet, label = "wavelet")
+plt.legend()
 
 plt.show()
